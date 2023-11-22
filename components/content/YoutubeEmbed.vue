d993918d8f265e87978108ede2483825a1fa00d1
isxander <xander@isxander.dev>
2023-11-22 22:41:22 +0000

    fix youtube embeds overflowing

--- components/content/YoutubeEmbed.vue
+++ components/content/YoutubeEmbed.vue
@@ -1,8 +1,10 @@
 <template>
-    <iframe width="560" height="315" :src="'https://www.youtube.com/embed/' + video"
-        title="YouTube video player" frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen></iframe>
+    <div class="youtube-container">
+        <iframe :src="'https://www.youtube.com/embed/' + video"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -10,3 +12,20 @@ const props = defineProps<{
     video: string
 }>()
 </script>
+
+<style scoped lang="scss">
+.youtube-container {
+    position: relative;
+    width: 100%;
+    padding-bottom: calc((9/16)*100%);
+    height: 0;
+
+    iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+}
+</style>
